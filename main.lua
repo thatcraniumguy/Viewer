d263585b14561e0f8c738310cb5ea18f1874daa8
Nick Wireman <nwireman@gmail.com>
2015-04-26 18:29:16 -0500

    Add function for obtaining settings info

--- main.lua
+++ main.lua
@@ -2,6 +2,28 @@
 -- A comprehensive peripheral visualizer for Minecraft
 version = 0.01
 
+function getSettings()
+    -- Retrieve settings from saved file, or if none exists, create settings from default list
+    local defaultSettings = {
+        menuBarColor = colors.red,
+        menuTextColor = colors.blue,
+        menuLocation = "top"
+        backgroundColor = colors.black,
+        textColor = colors.white
+        }
+    if not fs.exists("viewer.cfg") then
+        local file = fs.open("viewer.cfg", "w")
+        file.write(texutils.serialize(defaultSettings))
+        file.close()
+    else
+        local file = fs.open("viewer.cfg", "r")
+        local content = file.read()
+        settings = textutils.unserialize(content)
+    end
+end
+
+settings = getSettings()
+
 local pList = {}
 function getList()
     for num,pName in ipairs(peripheral.getNames()) do
