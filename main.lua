1c83e5c10c63834458806ee3012fce6eff0c22fc
Nick Wireman <nwireman@gmail.com>
2015-04-26 16:23:58 -0500

    Add main function for populating peripheral info

--- main.lua
+++ main.lua
@@ -3,15 +3,30 @@
 
 local pList = {}
 function getList()
-    for _,name in pairs(peripheral.getNames()) do
-        local p = peripheral.wrap(name)
-        pList.name = name
-        pList.name.type = p.getType()
+    for num,pName in ipairs(peripheral.getNames()) do
+        local p = peripheral.wrap(pName)
+        pList[num] = {
+            name = pName,
+            label = peripheral.getType(pName),
+            methods = p.listMethods(),
+            fluids = p.getTankInfo(),
+            inventory = {
+                name = p.getInventoryName(),
+                max = p.getInventorySize(),
+                stored = p.getAllStacks()
+                },
+            energy = {
+                max = p.getMaxEnergyStored(),
+                stored = p.getEnergyStored()
+                }
+            }
     end
 end
-
-function populateList(list)
-    for i,v in pairs(list) do
-        
-    end
+local function printPeripheralTable()
+    local file = fs.open("dump", "w")
+    file.write(textutils.serialize(pList))
+    file.close()
+    print("Peripheral list dumped!")
 end
+getList()
+printPeripheralTable()
