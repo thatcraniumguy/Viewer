8575e70401a4fcefa7ac6181f4ae9855f3ccab69
Nick Wireman <nwireman@gmail.com>
2015-04-26 20:10:56 -0500

    Logging tweaks

--- main.lua
+++ main.lua
@@ -6,13 +6,13 @@ version = 0.01
 os.loadAPI("gui")
 
 -- Clear log files
-if fs.exists("viewer.log") then
-    fs.delete ("viewer.log")
+if fs.exists("/logs/viewer.log") then
+    fs.delete ("/logs/viewer.log")
 end
 
 -- Debug tool
-local function logFile(input)
-    local file = fs.open("viewer.log", "a")
+local function logfile(input)
+    local file = fs.open("/logs/viewer.log", "a")
     file.write(input)
     file.close()
 end
@@ -20,23 +20,29 @@ end
 function getSettings()
     -- Retrieve settings from saved file, or if none exists, create settings from default list
     local defaultSettings = {
-        
+        safeShutdown = false
         }
     if not fs.exists("viewer.cfg") then
+        logfile("Main settings not found!")
         local file = fs.open("viewer.cfg", "w")
         file.write(texutils.serialize(defaultSettings))
         file.close()
+        logfile("Using default settings")
     else
+        logfile("Main settings found!")
         local file = fs.open("viewer.cfg", "r")
         local content = file.read()
-        settings = textutils.unserialize(content)
+        settings = textutils.serialize(content)
+        logfile("Main settings loaded")
     end
 end
 
 function getList()
     pList = {}
     for num,pName in ipairs(peripheral.getNames()) do
+        logfile(num.." available peripherals found:")
         local p = peripheral.wrap(pName)
+        logfile(pName)
         pList[num] = {
             name = pName,
             label = peripheral.getType(pName),
@@ -55,7 +61,6 @@ function getList()
     end
 end
 
-settings = getSettings()
 gui.drawScreen()
 
 -------------------------------------------------------------------
